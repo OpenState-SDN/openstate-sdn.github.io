f93d577d442a399c4b115803e56c3b14b2147410
Davide Sanvito <DavideSanvito@users.noreply.github.com>
2017-04-21 17:00:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -74,6 +74,7 @@ deleted. Are you sure? (y/n) " -n 1 -r
     # install ryu
     sudo pip install -r tools/pip-requires
     sudo pip install -I six==1.9.0
+    sudo pip install -I eventlet==0.17.4
     sudo python ./setup.py install
 
     sudo chown -R mininet:mininet ~/ryu
